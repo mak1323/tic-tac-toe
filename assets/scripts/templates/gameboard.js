2f43c8ef6b368fe01ada5d9b893c741732609813
mak1323 <myleskaeding@gmail.com>
2017-06-28 10:50:58 -0400

    Updating game logics
    I made a dictionary of winning conditions and a the gameboard.

--- assets/scripts/templates/gameboard.js
+++ assets/scripts/templates/gameboard.js
@@ -11,3 +11,46 @@ const gameboard = [
   [2, 1],
   [2, 2]
 ]
+
+const winningConditions = {
+  conditionOne: [
+    [0, 0],
+    [0, 1],
+    [0, 2]
+  ],
+  conditionTwo: [
+    [1, 0],
+    [1, 1],
+    [1, 2]
+  ],
+  conditionThree: [
+    [2, 0],
+    [2, 1],
+    [2, 2]
+  ],
+  conditionFour: [
+    [0, 0],
+    [1, 0],
+    [2, 0]
+  ],
+  conditionFive: [
+    [0, 1],
+    [1, 1],
+    [2, 1]
+  ],
+  conditionSix: [
+    [0, 2],
+    [1, 2],
+    [2, 2]
+  ],
+  conditionSeven: [
+    [0, 2],
+    [1, 1],
+    [2, 0]
+  ],
+  conditionEight: [
+    [0, 0],
+    [1, 1],
+    [2, 2]
+  ]
+}

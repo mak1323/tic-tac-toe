62135939c81305f13f6b9005a127497bc2b14ce0
mak1323 <myleskaeding@gmail.com>
2017-06-29 11:22:18 -0400

    Victory Engine is now running all cylinders
    I made my remaining brute force conditions, then put those into
    functions then put those functions into a function.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,6 +2,7 @@
 
 const getFormFields = require('../../lib/get-form-fields')
 const gameboardRef = require('./gameboard')
+const winners = require('./winningconditions')
 
 let playerOne = []
 
@@ -14,52 +15,18 @@ const positionToValue = function(currentBox) {
   }
 }
 
-// const checkVictory = function (arr) {
-//   const playerArray = arr
-//   console.log(playerArray)
-//   for (let j = 0; j < playerArray.length; j++) {
-//     for (let i = 0; i < gameboardRef.winningConditions.length; i++) {
-//       if (playerArray[j] === gameboardRef.winningConditions[i][0]) {
-//         if (playerArray[j] === gameboardRef.winningConditions[i][1]) {
-//           if (playerArray[j] === gameboardRef.winningConditions[i][2]) {
-//             console.log('Player Wins')
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-
 const playerMove = function(event) {
     event.preventDefault()
     console.log(this)
     const currentBox = this
     const currentMove = positionToValue(currentBox)
-    let checkOne = false
-    let checkTwo = false
-    let checkThree = false
+
     playerOne.push({
       'move': currentMove
     })
     console.log(playerOne)
     if (playerOne.length >= 3) {
-      console.log('bacon')
-      if (playerOne.find(gameboardRef.isWinZero)) {
-        checkOne = true
-        console.log(checkOne)
-      }
-      if (playerOne.find(gameboardRef.isWinOne)) {
-        checkTwo = true
-        console.log(checkTwo)
-      }
-      if (playerOne.find(gameboardRef.isWinTwo)) {
-        checkThree = true
-        console.log(checkThree)
-      }
-      console.log(checkOne, ' ', checkTwo, ' ', checkThree)
-      if (checkOne === true && checkTwo === true && checkThree === true) {
-        console.log('Victory')
-      }
+      winners.checkVictory(playerOne)
     }
   }
 
--- assets/scripts/gameboard.js
+++ assets/scripts/gameboard.js
@@ -40,75 +40,7 @@ const gameboardPosition = [{
   positionName: 'gameBoxBottomRight',
   id: [2, 2]
 }]
-const winningConditions = [
-  {
-    winOne: [0, 0],
-    winTwo: [0, 1],
-    winThree: [0, 2]
-  },
-  {
-    winOne: [1, 0],
-    winTwo: [1, 1],
-    winThree: [1, 2]
-  },
-  {
-    winOne: [2, 0],
-    winTwo: [2, 1],
-    winThree: [2, 2]
-  },
-  {
-    winOne: [0, 0],
-    winTwo: [1, 0],
-    winThree: [2, 0]
-  },
-  {
-    winOne: [0, 1],
-    winTwo: [1, 1],
-    winThree: [2, 1]
-  },
-  {
-    winOne: [0, 2],
-    winTwo: [1, 2],
-    winThree: [2, 2]
-  },
-  {
-    winOne: [0, 2],
-    winTwo: [1, 1],
-    winThree: [2, 0]
-  },
-
-  {
-    winOne: [0, 0],
-    winTwo: [1, 1],
-    winThree: [2, 2]
-  }
-]
-
-const isWinZero = (moves) => {
-  if (moves.move[0] === 0 && moves.move[1] === 0) {
-    console.log('working')
-    return true
-  }
-}
-
-const isWinOne = (moves) => {
-  if (moves.move[0] === 0 && moves.move[1] === 1) {
-    console.log('slot two working')
-    return true
-  }
-}
-
-const isWinTwo = (moves) => {
-  if (moves.move[0] === 0 && moves.move[1] === 2) {
-    console.log('slot three working')
-    return true
-  }
-}
 
 module.exports = {
-  winningConditions,
-  gameboardPosition,
-  isWinTwo,
-  isWinOne,
-  isWinZero
+  gameboardPosition
 }
